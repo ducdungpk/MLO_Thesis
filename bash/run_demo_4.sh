ad97f0c28f15ddd96263d903ca041daef0ca1a9d
ducdungpk <87109581+ducdungpk@users.noreply.github.com>
2026-08-20 19:36:49 +0700

    Update run_demo_4.sh

--- bash/run_demo_4.sh
+++ bash/run_demo_4.sh
@@ -9,7 +9,7 @@ echo "===== DEMO 4 (MLO Multi-STA Dual-Link) ====="
 # Load Sweep
 for load in 2 8 14 20 26 32 38 44 50 56 65 80 90
 do
-  ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=5 --offeredLoad=$load \
+  ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=5 --offeredLoad=$load --RngRun=1 \
   | tee /dev/tty \
   | grep "^RESULT," \
   | sed "s/RESULT,/load_sweep,/" >> $OUT
@@ -18,7 +18,7 @@ done
 # STA Scaling
 for sta in 1 5 7 9 11 13 15 17 20 25 30 35 40 50
 do
-  ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=$sta --offeredLoad=2.5 \
+  ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=$sta --offeredLoad=2.5 --RngRun=1 \
   | tee /dev/tty \
   | grep "^RESULT," \
   | sed "s/RESULT,/sta_scaling,/" >> $OUT
@@ -29,7 +29,7 @@ for sta in 15 20 25 30
 do
   for load in 80 90 100 110
   do
-    ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=$sta --offeredLoad=$load \
+    ./ns3 run demo-4-multi-sta-dual-link.cc -- --nSta=$sta --offeredLoad=$load --RngRun=1 \
     | tee /dev/tty \
     | grep "^RESULT," \
     | sed "s/RESULT,/stress,/" >> $OUT
